fdc3c5d8a90c805b886699cd8406e7c9fb9ebcc8
Joker <40009719+pic123123@users.noreply.github.com>
2023-09-11 07:32:25 +0900

    사진첨부끝

--- lib/features/post/camera_bottom_sheet.dart
+++ lib/features/post/camera_bottom_sheet.dart
@@ -1,3 +1,4 @@
+import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:nomad_flutter_twitter/constants/sizes.dart';
 import 'package:image_picker/image_picker.dart';
@@ -12,15 +13,56 @@ class CameraBottomSheet extends StatefulWidget {
 class _CameraBottomSheetState extends State<CameraBottomSheet> {
   XFile? _image; //이미지를 담을 변수 선언
   final ImagePicker picker = ImagePicker(); //ImagePicker 초기화
+  late CameraController controller;
+  List<CameraDescription> cameras = [];
 
-  //이미지를 가져오는 함수
+  @override
+  void initState() {
+    super.initState();
+    initCameras();
+  }
+
+  void initCameras() async {
+    // cameras = await availableCameras();
+
+    // controller = CameraController(
+    //   cameras[0],
+    //   ResolutionPreset.medium,
+    // );
+
+    // controller.initialize().then((_) {
+    //   if (!mounted) return;
+    //   setState(() {});
+    // });
+  }
+
+  Future<void> capturePicture() async {
+    try {
+      // final path =
+      //     join((await getTemporaryDirectory()).path, '${DateTime.now()}.png');
+      // await controller.takePicture(path);
+      // setState(() {
+      //   _image = XFile(path);
+      // });
+    } catch (e) {
+      print(e);
+    }
+  }
+
+// 이미지 가져오기 함수 수정
   Future getImage(ImageSource imageSource) async {
-    //pickedFile에 ImagePicker로 가져온 이미지가 담긴다.
-    final XFile? pickedFile = await picker.pickImage(source: imageSource);
-    if (pickedFile != null) {
-      setState(() {
-        _image = XFile(pickedFile.path); //가져온 이미지를 _image에 저장
-      });
+    if (imageSource == ImageSource.camera) {
+      // If the source is the camera, then use the camera package to take a photo.
+      capturePicture();
+    } else {
+      // Otherwise use the image picker to select a photo from the gallery.
+      final XFile? pickedFile = await picker.pickImage(source: imageSource);
+      if (pickedFile != null) {
+        setState(() {
+          _image = XFile(pickedFile.path); //가져온 이미지를 _image에 저장
+        });
+        return _image; //PostScreen으로 반환하기위해 Return이 필요함
+      }
     }
   }
 
@@ -46,13 +88,19 @@ class _CameraBottomSheetState extends State<CameraBottomSheet> {
               ListTile(
                 leading: const Icon(Icons.camera_alt_outlined),
                 title: const Text('사진 촬영하기'),
-                onTap: () => getImage(ImageSource.camera),
+                onTap: () async {
+                  final selectedImage = await getImage(ImageSource.camera);
+                  Navigator.pop(context, selectedImage);
+                },
               ),
               const Divider(color: Colors.black26, thickness: 1),
               ListTile(
                 leading: const Icon(Icons.photo_size_select_actual_outlined),
                 title: const Text('사진 선택하기'),
-                onTap: () => getImage(ImageSource.gallery),
+                onTap: () async {
+                  final selectedImage = await getImage(ImageSource.gallery);
+                  Navigator.pop(context, selectedImage);
+                },
               ),
             ],
           ),
--- lib/features/post/post_screen.dart
+++ lib/features/post/post_screen.dart
@@ -1,3 +1,6 @@
+import 'dart:io';
+
+import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:nomad_flutter_twitter/common/widgets/form_button.dart';
@@ -22,8 +25,10 @@ class _PostScreenState extends State<PostScreen> {
     print("123");
   }
 
+  File? _image; //이미지를 담을 변수 선언
+
   void _onMoveCameraScreen(BuildContext context) async {
-    await showModalBottomSheet(
+    final XFile? selectedImage = await showModalBottomSheet(
       context: context,
 
       /// bottom sheet의 사이즈를 바꿀 수 있게 해줌, (listView를 사용할거면 true)
@@ -31,6 +36,13 @@ class _PostScreenState extends State<PostScreen> {
       backgroundColor: Colors.transparent,
       builder: (context) => const CameraBottomSheet(),
     );
+    if (selectedImage != null) {
+      // Use your image here.
+      print("Selected Image Path : ${selectedImage.path}");
+      setState(() {
+        _image = File(selectedImage.path);
+      });
+    }
   }
 
   @override
@@ -112,8 +124,8 @@ class _PostScreenState extends State<PostScreen> {
               ),
               TextField(
                 controller: _contentsController,
-                maxLength: 400,
-                maxLines: 20,
+                maxLength: 300,
+                maxLines: 15,
                 autocorrect: false,
                 decoration: InputDecoration(
                   hintText: '내용을 입력해 주세요.',
@@ -139,6 +151,15 @@ class _PostScreenState extends State<PostScreen> {
                 ),
                 cursorColor: Theme.of(context).primaryColor,
               ),
+              _image == null
+                  ? const Text('')
+                  : Image.file(
+                      _image!,
+                      width: 200, // 이미지의 원하는 너비를 설정하세요.
+                      height: 150, // 이미지의 원하는 높이를 설정하세요.
+                      fit: BoxFit.cover,
+                    ),
+              Gaps.v10,
               GestureDetector(
                 onTap: () => _onSave(),
                 child: FormButton(
